f34c3c0c9766e0e584054eb2f07e95ffa2201d84
Aron Roberts <aron@socrates.berkeley.edu>
2014-05-16 13:45:09 -0700

    Perform builds across layers in consistent ordering.

--- update-cspace-src.sh
+++ update-cspace-src.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd $USER_HOME/$CSPACE_USERNAME/src/services && git checkout $COLLECTIONSPACE_GIT_BRANCH && git pull
 cd $USER_HOME/$CSPACE_USERNAME/src/application && git checkout $COLLECTIONSPACE_GIT_BRANCH && git pull
+cd $USER_HOME/$CSPACE_USERNAME/src/services && git checkout $COLLECTIONSPACE_GIT_BRANCH && git pull
 cd $USER_HOME/$CSPACE_USERNAME/src/ui && git checkout $COLLECTIONSPACE_GIT_BRANCH && git pull
 
 exit 0
